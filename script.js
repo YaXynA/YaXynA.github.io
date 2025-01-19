8add72129252f2f30c12d591735e4fec2d82c042
Joyka <Yaxymma@yandex.ru>
2025-01-19 21:51:35 +0300

    mms

--- script.js
+++ script.js
@@ -29,3 +29,27 @@ if ('serviceWorker' in navigator) {
         });
     });
 }
+
+
+let installPrompt = null;
+const installButton = document.querySelector("#install");
+
+window.addEventListener("beforeinstallprompt", (event) => {
+  event.preventDefault();
+  installPrompt = event;
+  installButton.removeAttribute("hidden");
+});
+
+installButton.addEventListener("click", async () => {
+  if (!installPrompt) {
+    return;
+  }
+  const result = await installPrompt.prompt();
+  console.log(`Install prompt was: ${result.outcome}`);
+  disableInAppInstallPrompt();
+});
+
+function disableInAppInstallPrompt() {
+  installPrompt = null;
+  installButton.setAttribute("hidden", "");
+}
